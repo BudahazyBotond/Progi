436a8a05932bb03e2d6a6b92256a65e35cffa54f
BudahazyBotond <botondbudahazi2007@gmail.com>
2024-08-28 12:42:23 +0200

    kezdes

---
+++ Kezdes/1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(age+" éves vagy");
+            Console.WriteLine("10 év mulva "+(age+10)+" leszel");
+            Console.Write("Add meg az első számot: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Add meg a második számot: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("A két szám külömbségének maradéka: ");
+            Console.WriteLine(num1%num2);*/
+            Console.Write("Add meg a számodat: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = 2;
+            if (num1 % num2==0) {
+                Console.WriteLine("A szám páros.");
+            }
+            else{
+                Console.WriteLine("A szám páratlan.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Kezdes/SzamoloGep/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzamoloGep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adja meg az első számot: ");
+            float num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adja meg a második számot: ");
+            float num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adja meg a műveletet(+ - * /): ");
+            string muv = Console.ReadLine();
+            
+            switch (muv)
+            {
+                case "+": Console.WriteLine("Az összeg "+(num1+num2));
+                    break;
+                case "-": Console.WriteLine("A különbség "+(num1-num2));
+                    break;
+                case "*": Console.WriteLine("A szorzat " + (num1 * num2));
+                    break;
+                case "/": 
+                    Console.WriteLine(Convert.ToDouble(num1 / num2));
+                    break;
+                default: Console.WriteLine("Rossz művelet");
+                    break;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
